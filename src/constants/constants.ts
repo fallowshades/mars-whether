4a48fc847b7b2acbf7c4b36108ed184f10cdb53c
Erik Jonsson <erik.jonsson@chas.se>
2024-11-25 18:59:56 +0100

    boiler plate set up

---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+export const Constants = {
+  TOKEN_KEY: '',
+  DOMAIN: '',
+  INTEGRATION_API_URL: '',
+  IOS_STAGING_CLIENT_ID:
+    '',
+  IOS_STAGING_CLIENT_SECRET:
+    '',
+
+  ANDROID_STAGING_CLIENT_ID:
+    '',
+  ANDROID_STAGING_CLIENT_SECRET:
+    '',
+};
